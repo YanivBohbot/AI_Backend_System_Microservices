faf6500f1e87e16ab1f1b8ab2723193d1539c87f
Yaniv Bohbot <yanivbohbot5@gmail.com>
2025-05-05 15:56:57 +0300

    add the docker files to containerize the microservices

--- log_service/app/routes/log_routes.py
+++ log_service/app/routes/log_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from ..models import LogEntry
 from ..services.logger_service import LogService
 
@@ -6,7 +6,13 @@ router = APIRouter(prefix="/logs")
 log_service = LogService()
 
 
+# @router.post("/", status_code=status.HTTP_201_CREATED)
+# def ingest_log(log: LogEntry):
+#     log_service.save_log(log)
+#     return {"status": "logged"}
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def ingest_log(log: LogEntry):
+def ingest_log(log: LogEntry, log_service: LogService = Depends()):
     log_service.save_log(log)
-    return {"status": "logged"}
+    return {"status": "logged to s3"}
--- log_service/app/services/logger_service.py
+++ log_service/app/services/logger_service.py
@@ -1,18 +1,32 @@
-from typing import List
 from app.models import LogEntry
 import logging
+import boto3
+import json
 
 logger = logging.getLogger("log-service")
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(asctime)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="[%(levelname)s] %(asctime)s - %(message)s"
+)
 
 # Simple in-memory store (can replace with DB later)
 
 
 class LogService:
     def __init__(self):
-        self.logs: List[LogEntry] = []
+        self.s3 = boto3.client("s3")
+        self.bucket_name = "logs"
 
-    def save_log(self, log: LogEntry):
-        self.logs.append(log)
-        logger.info(f"{log.service} | {log.event} | {log.message}")
-        print(f"[LOG] [{log.timestamp}] {log.service}: {log.message}", flush=True)
+    def save_log(self, loginput: LogEntry):
+        log_data = loginput.model_dump()
+        key = f"logs/{loginput.timestamp.strftime('%Y-%m-%dT%H-%M-%S')}_{loginput.service}.json"
+        self.s3.put_object(
+            Bucket=self.bucket_name,
+            Key=key,
+            Body=json.dumps(log_data),
+            ContentType="application/json",
+        )
+        print(f"[S3 LOG] Saved to {key}")
+
+        # self.logs.append(log)
+        # logger.info(f"{log.service} | {log.event} | {log.message}")
+        # print(f"[LOG] [{log.timestamp}] {log.service}: {log.message}", flush=True)
